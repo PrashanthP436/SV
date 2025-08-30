bf42fcd877422b753f7e36d2c00f6ad4b892ede7
PrashanthP436 <prashanthp436@gmail.com>
2025-08-30 15:49:26 +0530

    Update unpacked_arrays.sv

--- Datatypes/Unpacked_Arrays/unpacked_arrays.sv
+++ Datatypes/Unpacked_Arrays/unpacked_arrays.sv
@@ -1,7 +1,6 @@
 // 1. UnPacked Arrays(Fixed Size Arrays) has dimensions after variable declaration
 // 2. Unpacked arrays Does not evaluate to integral value. ex : byte a[5]; a = a+1 is illegal (a[2] = a[0] + 1 is legal as a[0] and a[2] are bytes)
 // No error on Out Of Bound reads or writes. Reads return default datatype values
-// for ex : bit [a:0][b:0][c:0] .....[z:0] p_array; can be accessed as p_array[a][b][c] ....[z] as per write or read requirement
 
 module unpacked_array;
   
@@ -12,29 +11,35 @@ module unpacked_array;
   initial begin
     unpack_array = '{default:'d12};
     unpack_array1 = '{'{default:'d45},'{default:'d11},'{default:'d34},'{default:'d67}};
+    
     unpack_array2 = '{'{'{default:'d45},'{default:'d11},'{default:'d34},'{default:'d67}},
                       '{'{default:'d56},'{default:'d09},'{default:'d87},'{default:'d00}}};
     
+    $display("=============== unpack_array 1D ====================");
     foreach(unpack_array[i]) begin
       $display("unpack_array[%0d] : %0b",i,unpack_array[i]);
     end
     
+    $display("=============== unpack_array1 2D ====================");
     foreach(unpack_array1[i]) begin
       $display("unpack_array1[%0d] : %0p",i,unpack_array1[i]);
     end
-                        
+    
+    $display("=============== unpack_array2 3D ====================");
     foreach(unpack_array2[i,j]) begin
       $display("unpack_array2[%0d] : %p, \nunpack_array2[%0d][%0d] : %p",i,unpack_array2[i],i,j,unpack_array2[i][j]);                          
     end
     
+    $display("=============== Print All Unpacked arrays =====================");
     $display("unpack_array : %p \n unpack_array1 : %0p, \n unpack_array2 : %0p",unpack_array,unpack_array1,unpack_array2);
-
+    $display("===============================================================");
   end
-  
+
 endmodule
 
-Output :
+Output : 
 
+=============== unpack_array 1D ====================
 unpack_array[0] : 1100
 unpack_array[1] : 1100
 unpack_array[2] : 1100
@@ -42,10 +47,12 @@ unpack_array[3] : 1100
 unpack_array[4] : 1100
 unpack_array[5] : 1100
 unpack_array[6] : 1100
+=============== unpack_array1 2D ====================
 unpack_array1[3] : '{45, 45, 45, 45, 45, 45, 45, 45} 
 unpack_array1[2] : '{11, 11, 11, 11, 11, 11, 11, 11} 
 unpack_array1[1] : '{34, 34, 34, 34, 34, 34, 34, 34} 
 unpack_array1[0] : '{67, 67, 67, 67, 67, 67, 67, 67} 
+=============== unpack_array2 3D ====================
 unpack_array2[1] : '{'{'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d}, '{'hb, 'hb, 'hb, 'hb, 'hb, 'hb, 'hb, 'hb}, '{'h22, 'h22, 'h22, 'h22, 'h22, 'h22, 'h22, 'h22}, '{'h43, 'h43, 'h43, 'h43, 'h43, 'h43, 'h43, 'h43}} , 
 unpack_array2[1][3] : '{'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d} 
 unpack_array2[1] : '{'{'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d}, '{'hb, 'hb, 'hb, 'hb, 'hb, 'hb, 'hb, 'hb}, '{'h22, 'h22, 'h22, 'h22, 'h22, 'h22, 'h22, 'h22}, '{'h43, 'h43, 'h43, 'h43, 'h43, 'h43, 'h43, 'h43}} , 
@@ -62,7 +69,9 @@ unpack_array2[0] : '{'{'h38, 'h38, 'h38, 'h38, 'h38, 'h38, 'h38, 'h38}, '{'h9, '
 unpack_array2[0][1] : '{'h57, 'h57, 'h57, 'h57, 'h57, 'h57, 'h57, 'h57} 
 unpack_array2[0] : '{'{'h38, 'h38, 'h38, 'h38, 'h38, 'h38, 'h38, 'h38}, '{'h9, 'h9, 'h9, 'h9, 'h9, 'h9, 'h9, 'h9}, '{'h57, 'h57, 'h57, 'h57, 'h57, 'h57, 'h57, 'h57}, '{'h0, 'h0, 'h0, 'h0, 'h0, 'h0, 'h0, 'h0}} , 
 unpack_array2[0][0] : '{'h0, 'h0, 'h0, 'h0, 'h0, 'h0, 'h0, 'h0} 
+=============== Print All Unpacked arrays =====================
 unpack_array : '{12, 12, 12, 12, 12, 12, 12}  
  unpack_array1 : '{'{45, 45, 45, 45, 45, 45, 45, 45}, '{11, 11, 11, 11, 11, 11, 11, 11}, '{34, 34, 34, 34, 34, 34, 34, 34}, '{67, 67, 67, 67, 67, 67, 67, 67}} , 
  unpack_array2 : '{'{'{'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d, 'h2d}, '{'hb, 'hb, 'hb, 'hb, 'hb, 'hb, 'hb, 'hb}, '{'h22, 'h22, 'h22, 'h22, 'h22, 'h22, 'h22, 'h22}, '{'h43, 'h43, 'h43, 'h43, 'h43, 'h43, 'h43, 'h43}}, '{'{'h38, 'h38, 'h38, 'h38, 'h38, 'h38, 'h38, 'h38}, '{'h9, 'h9, 'h9, 'h9, 'h9, 'h9, 'h9, 'h9}, '{'h57, 'h57, 'h57, 'h57, 'h57, 'h57, 'h57, 'h57}, '{'h0, 'h0, 'h0, 'h0, 'h0, 'h0, 'h0, 'h0}}} 
+===============================================================
            V C S   S i m u l a t i o n   R e p o r t 
